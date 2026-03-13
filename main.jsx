5aa5a1397e3c6a106590b44294ddc67ac6574384
jaymielke <jaymielke@me.com>
2026-03-12 21:13:03 -0400

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PracticePlanner from "./PracticePlanner.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PracticePlanner />
+  </React.StrictMode>
+);
